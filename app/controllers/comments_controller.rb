b741f97c120adf8fc1415e4c6e5e52e36eb4c007
Raouphy <albertgnae@hotmail.fr>
2018-02-01 21:57:30 +0100

    v2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,6 +24,13 @@ class CommentsController < ApplicationController
     else
       render 'edit'
     end
+
+    end
+  def destroy
+      @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
   private
